039df84cae5bd36d5cb2c59569e64542c21a6f29
Tom Dinh <tomdinhnz@gmail.com>
2016-11-01 19:26:58 +1300

    chage repo of IEnum into Linq5 dir

---
+++ LinqIEnumerable/MyLinq.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqIEnumerable
+{
+    public static class MyLinq
+    {
+        // extension methods
+        public static int MyCount<T>(this IEnumerable<T> sequence) // Generic type of T// invorke by 'this'
+        {
+            int c = 0;
+            foreach (var item in sequence)
+            {
+                c += 1;
+            }
+            return c;
+        }
+    }
+}
